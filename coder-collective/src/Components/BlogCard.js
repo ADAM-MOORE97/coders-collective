83310c690d7f891dc73e04421a5a55cccb6e715a
Adam <adammoore118@outlook.com>
2021-10-26 10:08:39 -0400

    initial css background/cards

--- coder-collective/src/Components/BlogCard.js
+++ coder-collective/src/Components/BlogCard.js
@@ -3,10 +3,12 @@ import React from 'react'
 
 function BlogCard({user,user:{blog_link,pictureUrl, username, id}}) {
     return (
-        <div style={{textAlign:'center'}} key={id}>
+        <div className="card" style={{textAlign:'center'}} key={id}>
+            <span>
             <h2>{username}</h2>
             <img src={pictureUrl}/>
             <p>{blog_link}</p>
+            </span>
         </div>
     )
 }
