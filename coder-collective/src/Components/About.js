2c7e34372099e6995e07c5ad198a6d03d1c26072
Adam <adammoore118@outlook.com>
2021-10-28 20:50:41 -0400

    about-branch

--- coder-collective/src/Components/About.js
+++ coder-collective/src/Components/About.js
@@ -1,9 +1,39 @@
+import styled from "styled-components";
+const About = () => {
+    return (
+        <AboutStyle>
+            <h4>What is Coders Collective?</h4>
+            <p>
+                Coders collective is a social posting site where software engineers and others in tech related fields can submit their professional blogs to the app.
+                This site allows visitors to gather to one place for access to writings from their favorite tech writers across the various platforms ideas and tutorials are
+                published upon. Visitors can choose to leave a 'like' ( ❤ ) on the platform; however, any comments or critiques towards user blog content are meant to be
+                published on their respective platforms. The field of computer science and in itself tech with regards to web/mobile applications is a field where everyone of all levels and experience can both learn and
+                provide knowledge to other developers. This collective knowledge is what pushes us and our peers to new heights in skill and discover.
+            </p>
+            <h6>This is <em style={{ color: '#a9cec2' }}>CODERS COLLECTIVE.</em></h6>
 
-const About = () =>{
-    return(
-        <div>
-            ABOUT
-        </div>
+        </AboutStyle>
     )
 }
 export default About;
+
+const AboutStyle = styled.div`
+text-align: center;
+margin-left: 50px;
+margin-right: 50px;
+ h4 {
+    color: #83a4a4;
+    font-size: 52px;
+    font-style: bold;
+   
+  };
+  p {
+      line-height: 100px;
+      color:#a9cec2 ;
+    font-size: 32px;
+  };
+  h6{
+    color: #83a4a4;
+    font-size: 42px;
+    word-spacing: 10px;
+  }`
--- coder-collective/src/Components/Home.js
+++ coder-collective/src/Components/Home.js
@@ -5,88 +5,90 @@ import styled from "styled-components";
 import Sort from "./Sort";
 import NewBlogger from "./NewBlogger"
 const Home = () => {
-// All States that affect rendered data & server requests
-    const [users, setUsers] = useState([]);
-    const [searchedData, setSearchedData] = useState(users);
-    const [like, setLike] = useState(false);
-    const [clicked, setClicked] = useState(false)
+  // All States that affect rendered data & server requests
+  const [users, setUsers] = useState([]);
+  const [searchedData, setSearchedData] = useState(users);
+  const [like, setLike] = useState(false);
+  const [clicked, setClicked] = useState(false)
 
-// GET
-    useEffect(() => {
-      fetch(`http://localhost:4000/coders`)
-        .then((r) => r.json())
-        .then((data) => setUsers(data))
-        .catch(() => console.log("Run Server!"));
-    }, [like]);
+  // GET
+  useEffect(() => {
+    fetch(`http://localhost:4000/coders`)
+      .then((r) => r.json())
+      .then((data) => setUsers(data))
+      .catch(() => console.log("Run Server!"));
+  }, [like]);
 
-// Patch
-    function fetchPatch(user) {
-      let userid = user.id;
-      let numLikes = user.likes;
-      fetch(`http://localhost:4000/coders/${userid}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ likes: numLikes + 1 }),
-      }).then(setLike((like) => !like));
-    }
+  // Patch
+  function fetchPatch(user) {
+    let userid = user.id;
+    let numLikes = user.likes;
+    fetch(`http://localhost:4000/coders/${userid}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ likes: numLikes + 1 }),
+    }).then(setLike((like) => !like));
+  }
 
-// Post
-    function addNewBlogger(formData) {
-      fetch("http://localhost:4000/coders/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(formData)
+  // Post
+  function addNewBlogger(formData) {
+    fetch("http://localhost:4000/coders/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
 
-      })
-        .then(setLike(like => !like))
-    }
+    })
+      .then(setLike(like => !like))
+  }
 
-// Search By Name
-    function handleSearch(e) {
-      const searchData = users.filter((user) => {
-        return user.username.toLowerCase().includes(e.target.value.toLowerCase());
-      });
-      setSearchedData(searchData);
-    }
+  // Search By Name
+  function handleSearch(e) {
+    const searchData = users.filter((user) => {
+      return user.username.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setSearchedData(searchData);
+  }
 
-// Set passed down Data to users after initial GET request
-    useEffect(() => {
-      setSearchedData(users);
-    }, [users]);
+  // Set passed down Data to users after initial GET request
+  useEffect(() => {
+    setSearchedData(users);
+  }, [users]);
 
-// Sort By Name Alphabetically
-    function handleSortClick() {
-      setClicked(!clicked)
-      if(clicked=== true){const sortedUsers = users.slice().sort((a, b) => {
+  // Sort By Name Alphabetically
+  function handleSortClick() {
+    setClicked(!clicked)
+    if (clicked === true) {
+      const sortedUsers = users.slice().sort((a, b) => {
         if (a.username > b.username) return 1;
         if (a.username < b.username) return -1;
         else return 0;
       });
-      setSearchedData(sortedUsers)}
-      else{setSearchedData(users)}
+      setSearchedData(sortedUsers)
     }
+    else { setSearchedData(users) }
+  }
 
-// All Child Components of HOME.js
-    return (
-      <div>
-        <Search handleSearch={handleSearch} users={users} />
-        <Sort handleSortClick={handleSortClick} />
-       <HomeCards>
+  // All Child Components of HOME.js
+  return (
+    <div>
+      <Search handleSearch={handleSearch} users={users} />
+      <Sort handleSortClick={handleSortClick} />
+      <HomeCards>
         {searchedData.map((user) => (
           <BlogCard fetchPatch={fetchPatch} key={user.id + 2} user={user} />
         ))}
-        </HomeCards>
-         <NewBlogger addNewBlogger={addNewBlogger} />
-      </div>
-    );
-  };
-  export default Home;
-  
-  const HomeCards = styled.div`
+      </HomeCards>
+      <NewBlogger addNewBlogger={addNewBlogger} />
+    </div>
+  );
+};
+export default Home;
+
+const HomeCards = styled.div`
   display: flex;
   flex-wrap: wrap;
   list-style: none;
